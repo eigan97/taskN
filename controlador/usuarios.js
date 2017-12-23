8ee4fafe9bd3e0a6d19a6bb291bbcd87fa788bd4
eigan97 <eigan@hotmail.com>
2017-12-23 03:10:30 -0600

    BNM,

--- controlador/usuarios.js
+++ controlador/usuarios.js
@@ -50,7 +50,7 @@ function validarSesion(){
 			var resp = JSON.parse(ajaxVal.responseText);
 			// alert(resp);
 			if (resp != 0) {
-				window.location.href = '../task/login.html';
+				window.location.href = '../taskN/login.html';
 				return false;
 			}
 		}
